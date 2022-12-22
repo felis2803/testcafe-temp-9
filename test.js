47a95cda65743429062b64a58a389c98e32de0a8
Ilya Afanasenko <fg1@mail.ru>
2022-12-22 16:37:48 +0400

    init

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ fixture `New Fixture`
     .page `localhost:3000`;
 
 test(`New Test`, async t => {
-    await ClientFunction(() => console.log('Test started:', Date.now()))();
+    await ClientFunction(() => console.log('TestCafe test started:', Date.now()))();
 
     await t.debug();
 });
